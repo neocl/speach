bea164365d70d3432103bcd872c0766fb72cd713
Le Tuan Anh <tuananh.ke@gmail.com>
2021-04-28 17:47:12 +0800

    update demo code

--- demo_elan.py
+++ demo_elan.py
@@ -4,26 +4,35 @@ from speach import elan
 eaf = elan.open_eaf('./test/data/test.eaf')
 
 # accessing metadata
+print("Accessing EAF Metadata")
+print("-" * 60)
 print(f"Author: {eaf.author} | Date: {eaf.date} | Format: {eaf.fileformat} | Version: {eaf.version}")
 print(f"Media file: {eaf.media_file}")
 print(f"Time units: {eaf.time_units}")
 print(f"Media URL: {eaf.media_url} | MIME type: {eaf.mime_type}")
 print(f"Media relative URL: {eaf.relative_media_url}")
 
-# accessing tiers & annotations
-for tier in eaf.tiers():
+# loop through all tiers in this eaf file
+print("\nLoop through all tiers")
+print("-" * 60)
+for tier in eaf:
     print(f"{tier.ID} | Participant: {tier.participant} | Type: {tier.type_ref}")
-    for ann in tier.annotations:
-        print(f"{ann.ID.rjust(4, ' ')}. [{ann.from_ts.ts} -- {ann.to_ts.ts}] {ann.value}")
+    # loop through all annotations in this tier
+    for ann in tier:
+        print(f"{ann.ID.rjust(4, ' ')}. [{ann.from_ts} :: {ann.to_ts}] {ann.text}")
 
-
-# test parsing EAF files with nested tiers
-elan2 = elan.open_eaf('./data/test_nested.eaf')
-# accessing nested tiers
+# loop through the root tiers only (i.e. ignored dependent tiers)
+print("\nLoop through root tiers only")
+print("-" * 60)
 for tier in eaf.roots:
-    print(f"{tier.ID} | Participant: {tier.participant} | Type: {tier.type_ref}")
-    print(f"  -- {ann.ID.rjust(4, ' ')}. [{ann.from_ts.ts} -- {ann.to_ts.ts}] {ann.value}")    
+    print(f"[+]-- {tier.ID} | Participant: {tier.participant} | Type: {tier.type_ref}")
+    for ann in tier:
+        print(f" |- {ann.ID.rjust(4, ' ')}. [{ann.from_ts} -- {ann.to_ts}] {ann.text}")
     for child_tier in tier.children:
-        print(f"    | {child_tier.ID} | Participant: {child_tier.participant} | Type: {child_tier.type_ref}")
-        for ann in child_tier.annotations:
-            print(f"    |- {ann.ID.rjust(4, ' ')}. [{ann.from_ts.ts} -- {ann.to_ts.ts}] {ann.value}")
+        print(f" |--[+]-- Child tier: {child_tier.ID} | Participant: {child_tier.participant} | Type: {child_tier.type_ref}")
+        for child_ann in child_tier:
+            # Dealing with ref annotation
+            if child_ann.ref:
+                print(f" .   |- {child_ann.ID.rjust(4, ' ')}. {child_ann.text} >> [#{child_ann.ref.ID}] `{child_ann.ref}`")
+            else:
+                print(f" .   |- {child_ann.ID.rjust(4, ' ')}. [{child_ann.from_ts} -- {child_ann.to_ts}] {child_ann.text}")
