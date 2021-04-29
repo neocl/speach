f2cc07d728759518fa9b0005d7afd2d53615bd0d
Le Tuan Anh <tuananh.ke@gmail.com>
2021-04-29 16:37:26 +0800

    update ELAN csv test case

--- test/test_elan.py
+++ test/test_elan.py
@@ -13,8 +13,9 @@ import os
 import unittest
 from pathlib import Path
 
-from speach import elan
+from chirptext import chio
 
+from speach import elan
 
 # -------------------------------------------------------------------------------
 # Configuration
@@ -77,49 +78,7 @@ class TestELAN(unittest.TestCase):
     def test_eaf_to_csv(self):
         eaf = read_eaf()
         actual = eaf.to_csv_rows()
-        expected = [('Person1 (Utterance)', 'P001', '1.040', '2.330', '1.290', 'How do you read this?'),
-                    ('Person1 (Utterance)', 'P001', '3.200', '5.050', '1.850', 'このリンゴ、おいしいね！'),
-                    ('Person1 (Utterance)', 'P001', '5.510', '6.350', '0.840', 'What does it mean?'),
-                    ('Person1 (Utterance)', 'P001', '7.070', '9.390', '2.320', 'It means "This apple is delicious".'),
-                    ('Person1 (Utterance)', 'P001', '9.670', '11.340', '1.670', '"この" means this'),
-                    ('Person1 (Utterance)', 'P001', '11.780', '13.110', '1.330', '"リンゴ" means "apple"'),
-                    ('Person1 (Utterance)', 'P001', '13.490', '16.090', '2.600', 'and "おいしい" means delicious.'),
-                    ('Person1 (Utterance)', 'P001', '16.615', '17.485', '0.870', 'Oh thanks'),
-                    ('marker', '', '0.830', '5.200', '4.370', 'convo start'),
-                    ('marker', '', '6.890', '16.260', '9.370', 'convo body'),
-                    ('marker', '', '16.523', '17.570', '1.047', 'convo end'),
-                    ('Person1 (Chunk)', 'P001', '1.040', '2.330', '1.290', 'How do you read this?'),
-                    ('Person1 (Chunk)', 'P001', '3.200', '5.050', '1.850', 'このリンゴ、おいしいね！'),
-                    ('Person1 (Chunk)', 'P001', '5.510', '6.350', '0.840', 'What does it mean?'),
-                    ('Person1 (Chunk)', 'P001', '7.070', '9.390', '2.320', 'It means "This apple is delicious".'),
-                    ('Person1 (Chunk)', 'P001', '9.731', '10.281', '0.550', 'この'),
-                    ('Person1 (Chunk)', 'P001', '10.554', '11.240', '0.686', 'means "this"'),
-                    ('Person1 (Chunk)', 'P001', '11.870', '12.303', '0.433', ' リンゴ'),
-                    ('Person1 (Chunk)', 'P001', '12.498', '13.041', '0.543', 'means "apple"'),
-                    ('Person1 (Chunk)', 'P001', '13.660', '13.915', '0.255', 'and'),
-                    ('Person1 (Chunk)', 'P001', '13.915', '14.711', '0.796', 'おいしい'),
-                    ('Person1 (Chunk)', 'P001', '14.882', '15.908', '1.026', 'means "delicious"'),
-                    ('Person1 (Chunk)', 'P001', '16.615', '17.485', '0.870', 'oh thanks'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'jp'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'jp'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'jp'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'jp'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (ChunkLanguage)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'jp'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en'),
-                    ('Person1 (Language)', 'P001', '', '', '', 'en')]
+        expected = [tuple(row) for row in chio.read_tsv("./test/data/test.eaf.csv")]
         self.assertEqual(expected, actual)
 
     def test_write_elan(self):
