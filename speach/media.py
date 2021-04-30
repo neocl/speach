bf9627d57fd80cce68d0bd4af0a4f740dc0bc405
Le Tuan Anh <tuananh.ke@gmail.com>
2021-04-30 17:24:13 +0800

    update media API: add cut(), concat(), convert() & validators

--- speach/media.py
+++ speach/media.py
@@ -16,6 +16,7 @@ import sys
 import logging
 import platform
 import subprocess
+import tempfile
 from pathlib import Path
 
 # ------------------------------------------------------------
@@ -74,12 +75,38 @@ def _ffmpeg(*args, ffmpeg_path=None, capture_output=False, text=None, check=Fals
         return output.decoding(encoding='utf-8') if text else output
 
 
+def _norm_path(p):
+    return os.path.expanduser(p) if p.startswith("~") else p
+
+
+def _validate_outfile(outfile):
+    _p = _norm_path(str(outfile))
+    if not outfile:
+        raise ValueError("Output file was not specified")
+    elif os.path.exists(_p):
+        raise FileExistsError(f"Output file {outfile} exists")
+    return _p
+
+
+def _validate_infile(infile):
+    _p = _norm_path(str(infile))
+    if not infile:
+        raise ValueError("Input file was not specified")
+    elif not os.path.isfile(_p):
+        raise FileNotFoundError(f"Input file {infile} does not exist")
+    return _p
+
+
+def _validate_args(infile, outfile):
+    return _validate_infile(infile), _validate_outfile(outfile)
+
+
 # ------------------------------------------------------------------------------
 # media APIs
 # ------------------------------------------------------------------------------
 
 def version(ffmpeg_path=None):
-    """ Determine using ffmpeg version 
+    """ Determine using ffmpeg version
 
     >>> from speach import media
     >>> media.version()
@@ -95,7 +122,7 @@ def version(ffmpeg_path=None):
 
 
 def locate_ffmpeg():
-    """ locate the binary file of ffmpeg program (i.e. ffmpeg.exe) 
+    """ locate the binary file of ffmpeg program (i.e. ffmpeg.exe)
 
     >>> from speach import media
     >>> media.locate_ffmpeg()
@@ -104,24 +131,78 @@ def locate_ffmpeg():
     return _FFMPEG_PATH
 
 
-def concat(*args, **kwargs):
-    raise NotImplementedError()
+def concat(text, outfile, dir=None, *args, **kwargs):
+    """ Process a ffmpeg demuxer file and write result to outfile
+
+    Read more: https://trac.ffmpeg.org/wiki/Concatenate
+
+    :param text: demuxer content string, which will be written to a temporary file before calling
+    :type text: str
+    :param outfile: path to an output file
+    :param dir: The directory to create the temp demuxer file, leave as None to use Python default temp dir
+    """
+    outfile = _validate_outfile(outfile)
+    with tempfile.TemporaryFile(mode="wt", dir=dir) as concat_file:
+        concat_file.write(text)
+        return _ffmpeg("-f", "concat",
+                       "-segment_time_metadata", 1,
+                       "-i", concat_file.name,
+                       "-af", "asetnsamples=32,aselect=concatdec_select",
+                       outfile, **kwargs)
 
 
-def cut(*args, **kwargs):
-    raise NotImplementedError()
+def cut(infile, outfile, from_ts=None, to_ts=None, use_concat=False, *args, **kwargs):
+    """ Cut a media file from a timestamp to another timestamp
+
+    To cut myfile.wav from ``00:03:12`` to the end of the file and write output to outfile.ogg
+
+    >>> media.cut("myfile.wav", "outfile.ogg", from_ts="00:03:12")
+
+    To cut myfile.wav from the beginning to ``00:04:27`` and then write output to outfile.ogg
+
+    >>> media.cut("myfile.wav", "outfile.ogg", to_ts="00:04:27")
+
+    When use_concat is set to True, both from_ts and to_ts must be specified
+
+    >>> media.cut("myfile.wav", "outfile.ogg", from_ts="00:03:12", to_ts="00:04:27", use_concat=True)
+
+    :param infile: Path to an existing file (in str or Path-like object)
+    :param outfile: Path to output file (must not exist, or else a FileExistsError will be raised)
+    :param from_ts: Leave as None to start cutting from the beginning
+    :type from_ts: a timestamp string or a TimeSlot object
+    :param to_ts: Timestamp to end cutting. Leave as None to cut to the end of the file
+    :type to_ts: a timestamp string or a TimeSlot object
+    :param use_concat: Set to True to use demuxer to cut audio file. Both from_ts and to_ts must be specified when use. Defaulted to None
+    """
+    infile, outfile = _validate_args(infile, outfile)
+    if from_ts is None and to_ts is None:
+        raise ValueError("from_ts and to_ts cannot be both None")
+    if use_concat and (from_ts is None or to_ts is None):
+        raise ValueError("when use_concat is True, both from_ts and to_ts must be defined")
+    if not use_concat:
+        # use -ss
+        if from_ts is None or str(from_ts) in ["0", "00:00:00", "00:00:00.000"]:
+            _ffmpeg("-i", infile, "-to", to_ts, outfile)
+        elif to_ts is not None:
+            _ffmpeg("-i", infile, "-ss", from_ts, "-to", to_ts, outfile)
+        else:
+            _ffmpeg("-i", infile, "-ss", from_ts, outfile)
+    else:
+        concat_text = "\n".join([f"file '{infile}'",
+                                 f"inpoint {from_ts}",
+                                 f"outpoint {to_ts}"])
+        concat(concat_text, outfile)
 
 
 def convert(infile, outfile, *args, ffmpeg_path=None):
-    """ Convert an audio/video file into another format """
-    if not infile:
-        raise ValueError("Input file was not specified")
-    elif not outfile:
-        raise ValueError("Output file was not specified")
-    elif not Path(infile).exists():
-        raise FileNotFoundError(f"Input file {infile} does not exist")
-    elif Path(outfile).exists():
-        raise FileNotFoundError(f"Output file {infile} exists")
-    else:
-        args = ["-i", str(infile), str(outfile)]
-        _ffmpeg(*args, ffmpeg_path=ffmpeg_path)
+    """ Convert an audio/video file into another format
+
+    To convert the file ``test.wav`` in Music folder under current user's home directory
+    into ``output.ogg``
+
+    >>> from speach import media
+    >>> media.convert("~/Music/test.wav", "~/Music/output.ogg")
+    """
+    infile, outfile = _validate_args(infile, outfile)
+    args = ["-i", str(infile), str(outfile)]
+    _ffmpeg(*args, ffmpeg_path=ffmpeg_path)
