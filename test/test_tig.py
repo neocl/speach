bd8d04d90ec2ba779f5c82a02c918d106fde07f0
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-17 11:24:57 +0800

    rename tig back to ttlig for backward compatible. Expose ttlig module as tig for future API

--- test/test_tig.py
+++ test/test_tig.py
@@ -20,8 +20,8 @@ from chirptext import deko
 from chirptext import dekomecab
 
 from speach import ttl
-from speach import tig
-from speach.tig import IGStreamReader, TTLTokensParser
+from speach import ttlig
+from speach.ttlig import IGStreamReader, TTLTokensParser
 
 
 # -------------------------------------------------------------------------------
@@ -58,8 +58,8 @@ class TestTokenizer(unittest.TestCase):
         sent = ttl.Sentence('It works.')
         token_string = 'It       works   .    '
         gloss_string = 'SUBJ     work    PUNC '
-        tokens = tig.tokenize(token_string)
-        glosses = tig.tokenize(gloss_string)
+        tokens = ttlig.tokenize(token_string)
+        glosses = ttlig.tokenize(gloss_string)
         sent.tokens = tokens
         for tk, gl in zip(sent.tokens, glosses):
             tk.new_tag(gl, tagtype='gloss')
@@ -70,12 +70,12 @@ class TestTokenizer(unittest.TestCase):
 
     def test_tokenizing_special_chars(self):
         ''' Only 2 characters - escapechar and delimiter '''
-        tokens = tig.tokenize('some\\ thing is a word .')
+        tokens = ttlig.tokenize('some\\ thing is a word .')
         expected = ['some thing', 'is', 'a', 'word', '.']
         self.assertEqual(tokens, expected)
         # last char is special
         with self.assertLogs("speach", "WARNING") as cm:
-            tokens = tig.tokenize('some\\ thing is a word \\.')
+            tokens = ttlig.tokenize('some\\ thing is a word \\.')
             expected = ['some thing', 'is', 'a', 'word', '.']
             self.assertEqual(tokens, expected)
             _found_warning = False
@@ -85,43 +85,43 @@ class TestTokenizer(unittest.TestCase):
                     break
             self.assertTrue(_found_warning)
         # last char is delimiter
-        self.assertRaises(Exception, lambda: tig.tokenize('this is wrong\\'))
+        self.assertRaises(Exception, lambda: ttlig.tokenize('this is wrong\\'))
 
 
 class TestFurigana(unittest.TestCase):
 
     def test_parse_furigana(self):
-        rubytext = tig.parse_furigana('')
+        rubytext = ttlig.parse_furigana('')
         self.assertEqual(str(rubytext), '')
         self.assertEqual(rubytext.to_html(), '')
 
-        self.assertRaises(ValueError, lambda: tig.parse_furigana(None))
+        self.assertRaises(ValueError, lambda: ttlig.parse_furigana(None))
 
-        rubytext = tig.parse_furigana('{食/た}べる')
+        rubytext = ttlig.parse_furigana('{食/た}べる')
         self.assertEqual(str(rubytext), '食べる')
         self.assertEqual(rubytext.to_html(), '<ruby><rb>食</rb><rt>た</rt></ruby>べる')
 
-        rubytext = tig.parse_furigana('{面/おも}{白/しろ}い')
+        rubytext = ttlig.parse_furigana('{面/おも}{白/しろ}い')
         self.assertEqual(str(rubytext), '面白い')
         self.assertEqual(rubytext.to_html(), '<ruby><rb>面</rb><rt>おも</rt></ruby><ruby><rb>白</rb><rt>しろ</rt></ruby>い')
 
-        rubytext = tig.parse_furigana('{漢字/かんじ}')
+        rubytext = ttlig.parse_furigana('{漢字/かんじ}')
         self.assertEqual(str(rubytext), '漢字')
         self.assertEqual(rubytext.to_html(), '<ruby><rb>漢字</rb><rt>かんじ</rt></ruby>')
 
-        rubytext = tig.parse_furigana('お{天/てん}{気/き}')
+        rubytext = ttlig.parse_furigana('お{天/てん}{気/き}')
         self.assertEqual(str(rubytext), 'お天気')
         self.assertEqual(rubytext.to_html(), 'お<ruby><rb>天</rb><rt>てん</rt></ruby><ruby><rb>気</rb><rt>き</rt></ruby>')
         # weird cases
-        rubytext = tig.parse_furigana('{{漢字/かんじ}}')
+        rubytext = ttlig.parse_furigana('{{漢字/かんじ}}')
         self.assertEqual(str(rubytext), '{漢字}')
         self.assertEqual(rubytext.to_html(), '{<ruby><rb>漢字</rb><rt>かんじ</rt></ruby>}')
         # %
-        rubytext = tig.parse_furigana('{％/パーセント}')
+        rubytext = ttlig.parse_furigana('{％/パーセント}')
         self.assertEqual(str(rubytext), '％')
         self.assertEqual(rubytext.to_html(), '<ruby><rb>％</rb><rt>パーセント</rt></ruby>')
 
-        rubytext = tig.parse_furigana('お{天/てん{気/き}')
+        rubytext = ttlig.parse_furigana('お{天/てん{気/き}')
         self.assertEqual(str(rubytext), 'お{天/てん気')  # first one won't be matched
         self.assertEqual(rubytext.to_html(), 'お{天/てん<ruby><rb>気</rb><rt>き</rt></ruby>')
 
@@ -134,7 +134,7 @@ class TestFurigana(unittest.TestCase):
         expected = '<ruby><rb>猫</rb><rt>ねこ</rt></ruby> が <ruby><rb>好</rb><rt>す</rt></ruby>き です 。'
         self.assertEqual(expected, actual)
         # test parse_ruby
-        actual = tig.make_ruby_html('ケーキ を {食/た}べた 。')
+        actual = ttlig.make_ruby_html('ケーキ を {食/た}べた 。')
         expected = 'ケーキ を <ruby><rb>食</rb><rt>た</rt></ruby>べた 。'
         self.assertEqual(expected, actual)
 
@@ -175,13 +175,13 @@ I have two cat-s.
     def test_read_header(self):
         inpath = VN_EXPLICIT
         with chio.open(inpath) as infile:
-            meta = tig.IGStreamReader._read_header(infile)
+            meta = ttlig.IGStreamReader._read_header(infile)
         expected = OrderedDict([('Language', 'Vietnamese'), ('Language code', 'vie'), ('Lines', 'orth translit gloss translat'), ('Author', 'Le Tuan Anh'), ('Date', 'May 25 2018')])
         self.assertEqual(meta, expected)
 
     def test_read_file(self):
         inpath = JP_MANUAL
-        s1, s2 = tig.read(inpath)
+        s1, s2 = ttlig.read(inpath)
         s1_json = {'text': '猫が好きです。', 'transliteration': 'neko ga suki desu .', 'transcription': '', 'morphtrans': '', 'morphgloss': 'cat SUBM likeable COP .', 'wordgloss': '', 'translation': 'I like cats.', 'ident': '01a_01', 'tokens': '{猫/ねこ} が {好/す}き です 。'}
         s2_json = {'text': '雨が降る。', 'transliteration': '', 'transcription': '', 'morphtrans': '', 'morphgloss': '', 'wordgloss': '', 'translation': 'It rains.', 'ident': '01a_02', 'tokens': '{雨/あめ} が {降/ふ}る 。'}
         self.assertEqual(s1.to_dict(), s1_json)
@@ -189,12 +189,12 @@ I have two cat-s.
         # test furigana
         s1_furi = '<ruby><rb>猫</rb><rt>ねこ</rt></ruby> が <ruby><rb>好</rb><rt>す</rt></ruby>き です 。'
         s2_furi = '<ruby><rb>雨</rb><rt>あめ</rt></ruby> が <ruby><rb>降</rb><rt>ふ</rt></ruby>る 。'
-        self.assertEqual(tig.make_ruby_html(s1.tokens), s1_furi)
-        self.assertEqual(tig.make_ruby_html(s2.tokens), s2_furi)
+        self.assertEqual(ttlig.make_ruby_html(s1.tokens), s1_furi)
+        self.assertEqual(ttlig.make_ruby_html(s2.tokens), s2_furi)
 
     def test_ttlig_auto(self):
         inpath = JP_IMPLICIT
-        sents = tig.read(inpath)
+        sents = ttlig.read(inpath)
         s = sents[0]
         self.assertEqual(s.text, '猫が好きです。')
         self.assertEqual(s.tokens, '{猫/ねこ} が {好/す}き です 。')
@@ -203,7 +203,7 @@ I have two cat-s.
 
     def test_ttlig_manual(self):
         inpath = JP_MANUAL
-        s1, s2 = tig.read(inpath)
+        s1, s2 = ttlig.read(inpath)
         s1_dict = {'text': '猫が好きです。', 'transliteration': 'neko ga suki desu .', 'transcription': '', 'morphtrans': '', 'morphgloss': 'cat SUBM likeable COP .', 'wordgloss': '', 'translation': 'I like cats.', 'ident': '01a_01', 'tokens': '{猫/ねこ} が {好/す}き です 。'}
         s2_dict = {'text': '雨が降る。', 'transliteration': '', 'transcription': '', 'morphtrans': '', 'morphgloss': '', 'wordgloss': '', 'translation': 'It rains.', 'ident': '01a_02', 'tokens': '{雨/あめ} が {降/ふ}る 。'}
         self.assertEqual(s1.to_dict(), s1_dict)
@@ -211,44 +211,44 @@ I have two cat-s.
 
     def test_read_empty_file(self):
         instream = io.StringIO('# TTLIG')
-        sents = tig.read_stream(instream)
+        sents = ttlig.read_stream(instream)
         self.assertEqual(sents, [])
 
     def test_read_invalid_ttlig(self):
         instream = io.StringIO('')
-        self.assertRaises(Exception, lambda: tig.read_stream(instream))
+        self.assertRaises(Exception, lambda: ttlig.read_stream(instream))
         invalid_file = os.path.join(TEST_DIR, 'data', 'testig_invalid.txt')
-        self.assertRaises(Exception, lambda: tig.read(invalid_file))
+        self.assertRaises(Exception, lambda: ttlig.read(invalid_file))
 
     @unittest.skipIf(not _MECAB_VERSION, "Deko is not available, test_make_furi_token is skipped.")
     def test_make_furi_token(self):
         s = deko.parse('友達')
         # f = ttlig.mctoken_to_furi(s[0])
-        f = tig.RubyToken.from_furi(s[0].surface, s[0].reading_hira())
+        f = ttlig.RubyToken.from_furi(s[0].surface, s[0].reading_hira())
         self.assertEqual(f.to_code(), '{友達/ともだち}')
         # half-width char
         s = deko.parse('0')
-        f = tig.RubyToken.from_furi(s[0].surface, s[0].reading_hira())
+        f = ttlig.RubyToken.from_furi(s[0].surface, s[0].reading_hira())
         self.assertEqual(f.to_code(), '0')
 
     @unittest.skipIf(not _MECAB_VERSION, "Deko is not available, test_tig_parsing is skipped.")
     def test_tig_parsing(self):
-        igrow = tig.text_to_igrow('友達と巡り会った。')
+        igrow = ttlig.text_to_igrow('友達と巡り会った。')
         self.assertEqual(igrow.text, '友達と巡り会った。')
         self.assertEqual(igrow.tokens, '{友達/ともだち} と {巡/めぐ}り{会/あ}っ た 。')
         expected = '<ruby><rb>友達</rb><rt>ともだち</rt></ruby> と <ruby><rb>巡</rb><rt>めぐ</rt></ruby>り<ruby><rb>会</rb><rt>あ</rt></ruby>っ た 。'
-        actual = tig.make_ruby_html(igrow.tokens)
+        actual = ttlig.make_ruby_html(igrow.tokens)
         self.assertEqual(expected, actual)
         # test
-        igrow = tig.text_to_igrow('言い尽くす')
+        igrow = ttlig.text_to_igrow('言い尽くす')
         self.assertEqual(igrow.text, '言い尽くす')
         self.assertEqual(igrow.tokens, '{言/い}い{尽/つ}くす')
         # test more
-        igrow = tig.text_to_igrow('言いなさい')
+        igrow = ttlig.text_to_igrow('言いなさい')
         self.assertEqual(igrow.text, '言いなさい')
         self.assertEqual(igrow.tokens, '{言/い}い なさい')
         # half-width char
-        igrow = tig.text_to_igrow('0時だ。')
+        igrow = ttlig.text_to_igrow('0時だ。')
         self.assertEqual(igrow.text, '0時だ。')
         self.assertEqual(igrow.tokens, '0 {時/じ} だ 。')
 
