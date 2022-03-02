1f7f95b4ebdef10c92af36610ee21581e1f06ead
Le Tuan Anh <tuananh.ke@gmail.com>
2022-03-02 16:58:16 +0800

    add blank eaf template for creating EAF

--- speach/data/__init__.py
+++ speach/data/__init__.py
@@ -2,3 +2,4 @@ import os
 
 MY_DIR = os.path.dirname(os.path.realpath(__file__))
 INIT_TTL_SQLITE = os.path.join(MY_DIR, 'scripts', 'init_corpus.sql')
+ELAN_BLANK_FILE = os.path.join(MY_DIR, 'elan', 'blank.eaf')
